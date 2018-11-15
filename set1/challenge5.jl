73ca5ee90a3251c21c26e37facdf6409bc6cb31a
Nirmal <nirmalvettiankal@gmail.com>
2018-11-14 21:07:39 -0500

    challenge 5

---
+++ set1/challenge5.jl
@@ -0,0 +1,44 @@
+#=
+
+Here is the opening stanza of an important work of the English language:
+
+Burning 'em, if you ain't quick and nimble
+I go crazy when I hear a cymbal
+Encrypt it, under the key "ICE", using repeating-key XOR.
+
+It should come out to:
+
+0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272
+a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f
+
+=#
+
+
+#=
+
+Takes a string and applies a repeating key xor with the xor key provided
+
+=#
+function repeating_key_xor(input::String, xor_key::String, retBytes=false::Bool)
+    input = Vector{UInt8}(input)
+    xor_key = Vector{UInt8}(xor_key)
+    xor_key_length = length(xor_key)
+    output = UInt8[]
+    for i in 1:length(input)
+        push!(output, xor(input[i], xor_key[((i - 1) % xor_key_length) + 1]))
+    end
+    if(retBytes)
+        return output
+    else
+        return String(output)
+    end
+end
+
+function main()
+    input = "Burning 'em, if you ain't quick and nimble I go crazy when I hear a cymbal"
+    xor_key = "ICE"
+    output = repeating_key_xor(input, xor_key, true)
+    println(bytes2hex(output))
+end
+
+# main()
